0f07e649c884af8749a2b37b51f86c5205447149
Marcelo Del Valle <cheloxnz@gmail.com>
2020-10-20 00:55:16 -0300

    Más algoritmos

--- Exercises.js
+++ Exercises.js
@@ -393,7 +393,35 @@ function findMultiples(int, limit) {
 
 // ---------------------------------------------------------------------- //
 
+// Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
+
+// Your task is to make 'Past' function which returns time converted to milliseconds.
+
+// Example:
+// past(0, 1, 1) == 61000
+// Input constraints: 0 <= h <= 23, 0 <= m <= 59, 0 <= s <= 59
+
+function past(h, m, s) {
+    //#Happy Coding! ^_^
+    return ((h * 3600) + (m * 60) + s) * 1000;
+}
+
 // ---------------------------------------------------------------------- //
+
+// Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+// Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+
+// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//    7      6      5      4      3            2      1
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+// Note: there will always be exactly one wolf in the array.
+
+function warnTheSheep(queue) {
+    const position = queue.reverse().indexOf('wolf');
+    return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${position}! You are about to be eaten by a wolf!`;
+}
 
 // ---------------------------------------------------------------------- //
 
